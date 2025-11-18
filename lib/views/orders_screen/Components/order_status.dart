d3c5feb37f7a099c3abba66bec225414a08143bb
Admin <shreeyashshinde76@gmail.com>
2025-11-18 11:57:24 +0530

    Updated code!! 17-11-2025

--- lib/views/orders_screen/Components/order_status.dart
+++ lib/views/orders_screen/Components/order_status.dart
@@ -1,19 +1,28 @@
 import 'package:myapp/consts/consts.dart';
 
-Widget orderStatus({icon,color,title,showDone}){
+Widget orderStatus({icon, color, title, showDone}) {
   return ListTile(
-    leading: Icon(icon, color: color,).box.border(color: color).roundedSM.padding(EdgeInsets.all(4)).make(),
-
+    leading: Icon(
+      icon,
+      color: color,
+    ).box.border(color: color).roundedSM.padding(const EdgeInsets.all(4)).make(),
     trailing: SizedBox(
-      height: 100,width: 120,
+      height: 100,
+      width: 120,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        "$title".text.color(darkFontGrey).make(),
-        showDone ? Icon(Icons.done, color: redColor,
-        ): Container(),
-      ],
-    ),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: "$title".text.color(darkFontGrey).make(),
+          ),
+          showDone
+              ? const Icon(
+                  Icons.done,
+                  color: redColor,
+                )
+              : Container(),
+        ],
+      ),
     ),
   );
 }
